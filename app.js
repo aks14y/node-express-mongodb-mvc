b118ec6afe0150abf00bbfe7ce41e0a52df36304
AXA77 <AXA77@acuitysso.com>
2024-02-16 19:32:37 +0530

    setup with blog module

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const blogRoutes = require("./routes/blogRoutes");
+
+// connect to mongodb
+const dbURI =
+  "mongodb+srv://akshay4362847:Akshay123@nodebeginner.wh5tq21.mongodb.net/node-beginner?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// register view engines
+app.set("view engine", "ejs");
+
+// static files
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
+// middleware
+app.use((req, res, next) => {
+//   console.log("host :", req.hostname);
+  next(); // to move the control to the next step
+});
+
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
+});
+
+app.use("/blogs", blogRoutes);
+
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
